c8910940fe29c794fa1aaa007d05e9ecb45b0c99
Venkata Reddy <vrfrnds630@gmail.com>
2026-07-21 04:48:50 +0000

    change in the code

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -3,7 +3,7 @@ package com.devops;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to DevOps Practice Project!");
+        System.out.println("Welcome to DevOps Project!");
         System.out.println("Java + Maven + Jenkins + SonarQube + Nexus + Docker + Kubernetes");
     }
 }
